93282b17e7d8f0dd45319651abdca76f589e9e3c
Eladzy <yacovski551@gmail.com>
2020-04-01 03:49:04 +0300

    1

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const Navbar = () => {
-    return (
+class Navbar extends Component {
+    componentDidMount() {
+        let sidenav = document.querySelectorAll('#mobile-links');
+        M.Sidenav.init(sidenav, {});
+    }
+    render() {
+        return (
 
-        <div className="constainer">
-            <nav className='nav-wrapper blue darken-4 '>
-                <div className="container">
-                    <a className="brand-logo">FlightCom</a>
-                    <a className="sidebar-trigger" data-target="mobile-links"></a>
-                    <ul className="right hide-on-med-and-down">
-                        <li><Link to='/Home'>Home</Link></li>
-                        <li><Link to='/flightSearch'>Search flight</Link></li>
-                        <li><Link to='/flightIndex'>flight index</Link></li>
-                        <li><NavLink to='/Departures'>Departures/Arrivals</NavLink></li>
-                        <li><NavLink to='/About'>About</NavLink></li>
-                        <li><NavLink to='/AccountOptions'>My Account</NavLink></li>
-                        <li><NavLink to='/signUpCustomer'>Signup</NavLink></li>
-                        <li><NavLink to='/signIn'>Sign in</NavLink></li>
-                    </ul>
-                </div>
-            </nav>
-            <ul className="sidenav" id="mobile-links">
-                <li><Link to='/Home'>Home</Link></li>
-                <li><NavLink to='/Departures'>Departures/Arrivals</NavLink></li>
-                <li><NavLink to='/About'>About</NavLink></li>
-                <li><NavLink to='/AccountOptions'>My Account</NavLink></li>
-                <li><NavLink to='/signUpCustomer'>Signup</NavLink></li>
-            </ul>
-        </div>
-    );
+            <div>
+                <nav className='nav-wrapper blue darken-4 '>
+                    <div className="container">
+                        <a className="brand-logo left">FlightCom</a>
+                        <ul className="right hide-on-med-and-down">
+                            <li><Link to='/Home'>Home</Link></li>
+                            <li><Link to='/flightSearch'>Search flight</Link></li>
+                            <li><Link to='/flightIndex'>flight index</Link></li>
+                            <li><NavLink to='/Departures'>Departures/Arrivals</NavLink></li>
+                            <li><NavLink to='/About'>About</NavLink></li>
+                            <li><NavLink to='/AccountOptions'>My Account</NavLink></li>
+                            <li><NavLink to='/signUpCustomer'>Signup</NavLink></li>
+                            <li><NavLink to='/signIn'>Sign in</NavLink></li>
+                        </ul>
+                        <a className="sidenav-trigger right hide-on-med-and-up" data-target="mobile-links"><i class="material-icons">menu</i></a>
+                    </div>
+                </nav>
+                <ul className="sidenav" id="mobile-links">
+                    <li><Link to='/Home'>Home</Link></li>
+                    <li><NavLink to='/Departures'>Departures/Arrivals</NavLink></li>
+                    <li><NavLink to='/About'>About</NavLink></li>
+                    <li><NavLink to='/AccountOptions'>My Account</NavLink></li>
+                    <li><NavLink to='/signUpCustomer'>Signup</NavLink></li>
+                </ul>
+            </div>
+        );
+    }
 }
-
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
 export default Navbar;
--- src/Components/flightSearch.js
+++ src/Components/flightSearch.js
@@ -5,6 +5,8 @@ import { getAirlines } from '../Actions/airlinesActions';
 import { getCountries } from '../Actions/countriesActions';
 import { bindActionCreators } from 'redux';
 import { Select, DatePicker, Button } from 'react-materialize';
+import { pointerBlue } from '../img/pointerBlueT.png'
+import { airplaneBlue } from '../img/airplaneBlueT.png'
 
 
 class flightSearch extends Component {
@@ -16,15 +18,12 @@ class flightSearch extends Component {
 
     onSubmitHandle = (e) => {
         e.preventDefault();
-
-
-
-        let params = ['' ,document.getElementById("airlineSelect").value,
+        let params = ['', document.getElementById("airlineSelect").value,
             document.getElementById("originSelect").value, document.getElementById("destinationSelect").value,
             document.getElementById("departureTimePicker").value, ''];
-            console.log("handle submit");
-            console.log(document.getElementById("airlineSelect").value);
-            console.log(params);
+        console.log("handle submit");
+        console.log(document.getElementById("airlineSelect").value);
+        console.log(params);
         this.props.searchFlights(params)
     }
 
@@ -32,136 +31,140 @@ class flightSearch extends Component {
         const countries = this.props.countries;
         const airlines = this.props.airlines;
         const flights = this.props.flights;
+        const searchedFlight = this.props.searchFlights
         return (
             //add auto complete  flight id's
-            <div className="row" >
-                <form className='white col' onSubmit={this.onSubmitHandle}>
-                    <div className="input-field col 12s 6m">
-                        <Select id="airlineSelect"  value={Option.value} >
-                            <option  ></option>
-                            {airlines.map(a =>
+            <div className="container">
+                <div className="row" >
+                    <form className='white col' onSubmit={this.onSubmitHandle}>
+                        <div className="input-field col 12s 6m">
+                            <Select id="airlineSelect" value={Option.value} >
+                                <option  ></option>
+                                {airlines.map(a =>
 
-                                (<option value={a.id} key={a.id}>{a.name}</option>)
-                            )}
-                        </Select>
-                        <label > Airline company</label>
-                    </div>
-                    <div className="input-field col 12s 6m">
-                        <Select id="originSelect" value={Option.value} >
-                            <option  ></option>
-                            {countries.map(c =>
-                                (<option value={c.Id} key={c.Id}>{c.Country_Name}</option>)
-                            )}
-                        </Select>
-                        <div className="container">
-
+                                    (<option value={a.id} key={a.id}>{a.name}</option>)
+                                )}
+                            </Select>
+                            <label > Airline company</label>
                         </div>
-                        <label > From</label>
-                    </div>
-                    <div className="input-field col 12s 6m">
-                        <Select id="destinationSelect" value={Option.value} >
-                            <option ></option>
-                            {countries.map(c =>
-                                (<option value={c.Id} key={c.Id}>{c.Country_Name}</option>)
-                            )}
-                        </Select>
-                        <label > To</label>
-                    </div>
-                    <div className="input-field col 12s 6m">
-                        <DatePicker
-                            id="departureTimePicker"
-                            options={{
-                                autoClose: false,
-                                container: null,
-                                defaultDate: null,
-                                disableDayFn: null,
-                                disableWeekends: false,
-                                events: [],
-                                firstDay: 0,
-                                format: 'mmm dd, yyyy',
-                                i18n: {
-                                    cancel: 'Cancel',
-                                    clear: 'Clear',
-                                    done: 'Ok',
-                                    months: [
-                                        'January',
-                                        'February',
-                                        'March',
-                                        'April',
-                                        'May',
-                                        'June',
-                                        'July',
-                                        'August',
-                                        'September',
-                                        'October',
-                                        'November',
-                                        'December'
-                                    ],
-                                    monthsShort: [
-                                        'Jan',
-                                        'Feb',
-                                        'Mar',
-                                        'Apr',
-                                        'May',
-                                        'Jun',
-                                        'Jul',
-                                        'Aug',
-                                        'Sep',
-                                        'Oct',
-                                        'Nov',
-                                        'Dec'
-                                    ],
-                                    nextMonth: '›',
-                                    previousMonth: '‹',
-                                    weekdays: [
-                                        'Sunday',
-                                        'Monday',
-                                        'Tuesday',
-                                        'Wednesday',
-                                        'Thursday',
-                                        'Friday',
-                                        'Saturday'
-                                    ],
-                                    weekdaysAbbrev: [
-                                        'S',
-                                        'M',
-                                        'T',
-                                        'W',
-                                        'T',
-                                        'F',
-                                        'S'
-                                    ],
-                                    weekdaysShort: [
-                                        'Sun',
-                                        'Mon',
-                                        'Tue',
-                                        'Wed',
-                                        'Thu',
-                                        'Fri',
-                                        'Sat'
-                                    ]
-                                },
-                                isRTL: false,
-                                maxDate: null,
-                                minDate: null,
-                                onClose: null,
-                                onDraw: null,
-                                onOpen: null,
-                                onSelect: null,
-                                parse: null,
-                                setDefaultDate: false,
-                                showClearBtn: false,
-                                showDaysInNextAndPreviousMonths: false,
-                                showMonthAfterYear: false,
-                                yearRange: 10
-                            }}
-                        />
-                        <label htmlFor="departureTimePicker">Departs on</label>
-                    </div>
-                    <div className="input-field col 12s 6m">
-                        <Button className="blue darken-4" waves="light" style={{ marginTop: "20px" }} >Go</Button>
-                    </div>
-                </form>
+                        <div className="input-field col 12s 6m">
+                            <Select id="originSelect" value={Option.value} >
+                                <option  ></option>
+                                {countries.map(c =>
+                                    (<option value={c.Id} key={c.Id}>{c.Country_Name}</option>)
+                                )}
+                            </Select>
+                            <div className="container">
+
+                            </div>
+                            <label > From</label>
+                        </div>
+                        <div className="input-field col 12s 6m">
+                            <Select id="destinationSelect" value={Option.value} >
+                                <option ></option>
+                                {countries.map(c =>
+                                    (<option value={c.Id} key={c.Id}>{c.Country_Name}</option>)
+                                )}
+                            </Select>
+                            <label > To</label>
+                        </div>
+                        <div className="input-field col 12s 6m">
+                            <DatePicker
+                                id="departureTimePicker"
+                                options={{
+                                    autoClose: false,
+                                    container: null,
+                                    defaultDate: null,
+                                    disableDayFn: null,
+                                    disableWeekends: false,
+                                    events: [],
+                                    firstDay: 0,
+                                    format: 'mmm dd, yyyy',
+                                    i18n: {
+                                        cancel: 'Cancel',
+                                        clear: 'Clear',
+                                        done: 'Ok',
+                                        months: [
+                                            'January',
+                                            'February',
+                                            'March',
+                                            'April',
+                                            'May',
+                                            'June',
+                                            'July',
+                                            'August',
+                                            'September',
+                                            'October',
+                                            'November',
+                                            'December'
+                                        ],
+                                        monthsShort: [
+                                            'Jan',
+                                            'Feb',
+                                            'Mar',
+                                            'Apr',
+                                            'May',
+                                            'Jun',
+                                            'Jul',
+                                            'Aug',
+                                            'Sep',
+                                            'Oct',
+                                            'Nov',
+                                            'Dec'
+                                        ],
+                                        nextMonth: '›',
+                                        previousMonth: '‹',
+                                        weekdays: [
+                                            'Sunday',
+                                            'Monday',
+                                            'Tuesday',
+                                            'Wednesday',
+                                            'Thursday',
+                                            'Friday',
+                                            'Saturday'
+                                        ],
+                                        weekdaysAbbrev: [
+                                            'S',
+                                            'M',
+                                            'T',
+                                            'W',
+                                            'T',
+                                            'F',
+                                            'S'
+                                        ],
+                                        weekdaysShort: [
+                                            'Sun',
+                                            'Mon',
+                                            'Tue',
+                                            'Wed',
+                                            'Thu',
+                                            'Fri',
+                                            'Sat'
+                                        ]
+                                    },
+                                    isRTL: false,
+                                    maxDate: null,
+                                    minDate: null,
+                                    onClose: null,
+                                    onDraw: null,
+                                    onOpen: null,
+                                    onSelect: null,
+                                    parse: null,
+                                    setDefaultDate: false,
+                                    showClearBtn: false,
+                                    showDaysInNextAndPreviousMonths: false,
+                                    showMonthAfterYear: false,
+                                    yearRange: 10
+                                }}
+                            />
+                            <label htmlFor="departureTimePicker">Departs on</label>
+                        </div>
+                        <div className="input-field col 12s 6m">
+                            <Button className="blue darken-4" waves="light" style={{ marginTop: "20px" }} >Go</Button>
+                        </div>
+                    </form>
+                </div>
+
             </div>
         );
     }
@@ -183,18 +186,7 @@ const mapDispatchToProps = (dispatch) => {
         getAllFlights: bindActionCreators(getAllFlights, dispatch),
         getAirlines: bindActionCreators(getAirlines, dispatch),
         getCountries: bindActionCreators(getCountries, dispatch),
-        // searchFlights: bindActionCreators(searchFlights, dispatch),
         searchFlights: (onSubmitHandle) => dispatch(searchFlights(onSubmitHandle))
-        // getAllFlights:()=>dispatch(getAllFlights())
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(flightSearch);
-
-// function populateSelectAirline(id, list = []) {
-//     let element = document.getElementById(id);
-//     list.map(item => element.options.add(new Option(item.name, item.id)));
-// }
-// function populateSelectCountry(id,list=[]){
-//     let element = document.getElementById(id);
-//     list.map(item=>element.Options.add(new Option(item.Id,item.Country_Name)));
-// }
