2ea2e7e2fd89ccabcc7c0faeecf29b784d3675d5
Eladzy <yacovski551@gmail.com>
2021-02-11 19:25:16 +0200

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,16 +3,16 @@ import { Carousel } from 'react-materialize'
 import seatsC from '../img/seatsC.jpg'
 const Home = () => {
     return (
-        
-        <Carousel 
+
+        <Carousel
             carouselId="Carousel-2"
             images={[
                 seatsC,
                 'img\seatsC.jpg',
                 '../img\sunsetC.jpg',
-                'https://picsum.photos/250/250?image=0',
+
             ]}
-           
+
             options={{
                 fullWidth: true,
                 indicators: true
